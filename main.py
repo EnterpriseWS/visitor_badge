001b10763e15a169b8e1f26534f7be4cc7144ae9
Robert Wang <robert.wang@enterprisews.com>
2021-04-08 15:57:08 -0700

    Changed main app content for uWSGI paramter format

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import printer_listener
+from printer_listener import app
 
 # ------------------------------------------------------
 # Use main.py to run on any WSGI server
 # Example for manually run gunicorn with shell command:
 #   $ uwsgi --http 0.0.0.0:8080 --module main:app
 # ------------------------------------------------------
-app = printer_listener.app
+
+if __name__ == "__main__":
+    app.run()
